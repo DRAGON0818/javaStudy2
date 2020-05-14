f17c608ab008c19a76cc8b1dc8b3a52ac8cdd3c5
DRAGON0818 <819698897@qq.com>
2020-05-14 13:23:24 +0800

    第九章完成

---
+++ out/test/javaStudy2/src/com/dlx/chapter09/SortAndShuffle/shuffleTest.java
@@ -0,0 +1,22 @@
+package com.dlx.chapter09.SortAndShuffle;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class shuffleTest {
+    public static void main(String[] args) {
+        List<Integer> numbers=new ArrayList<>();
+        for(int i=0;i<49;i++){
+             numbers.add(i);
+        }
+        Collections.shuffle(numbers);
+        List<Integer> list = numbers.subList(0, 6);
+        list.sort(Collections.reverseOrder());
+        System.out.println(list);
+        Collections.sort(numbers);
+        int i = Collections.binarySearch(numbers, 99);
+        System.out.println(i);
+        
+    }
+}
